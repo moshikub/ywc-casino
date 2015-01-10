1d317b4e9a1ebd8e808531498091d6f792d20b50
mamieoiiz <chaowanee@hotmail.com>
2015-01-10 11:00:58 +0700

    complete

--- nam-tao-pu-pla/style.js
+++ nam-tao-pu-pla/style.js
@@ -1,16 +1,20 @@
 window.i=0;
 window.n=1;
 window.random;
+window.setValue;
 
 $(document).ready(function(){
   $(".youwin").hide()
   $(".youlost").hide()
+  $(".select img").eq(0).addClass("active");
 });
 
-$(document).on("click", ".select img",function(){
-  $(this).siblings().removeClass("active");
-  $(this).parent().siblings().find("img").removeClass("active");
-  $(this).addClass("active");
+$(document).on("click", ".select img",function(){  
+  if ($(".play").hasClass("disable") == false) {
+    $(this).siblings().removeClass("active");
+    $(this).parent().siblings().find("img").removeClass("active");
+    $(this).addClass("active");
+  }
   
 });
 
@@ -25,6 +29,31 @@ $(document).on("click", ".play",function(){
     $(".play").addClass("disable")
     $(".play").attr("disabled","disabled");
     random = makeid()
+});
+
+$(document).on("keydown" ,function(event){
+  var i=1;
+  while (i<=6){
+    var active=$(".select img").eq(i-1).hasClass("active")
+    if (active == true){
+      if(event.keyCode == 67 || event.keyCode == 99) {
+        window.random = i;
+      }
+      else if(event.keyCode == 86 || event.keyCode == 118) {
+        if (i == 6) {
+          window.random = i-1
+        }
+        else if (i == 1) {
+          window.random = i+1
+        }
+        else {
+          window.random = window.random + 1;
+        }
+      }
+    }
+    i++;
+    console.log(window.random + "window")
+  }  
 });
 
 function focusFast(i,n,time){ 
